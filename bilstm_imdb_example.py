c0c076156ef8a7ba3b0e407ea6582f2fbf0f2e9e
Eric Berquist <eric.berquist@gmail.com>
2019-08-21 14:07:46 -0400

    constants should be SCREAMING_SNAKE_CASE

--- bilstm_imdb_example.py
+++ bilstm_imdb_example.py
@@ -6,22 +6,24 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Embedding, LSTM, Bidirectional
 from keras.datasets import imdb
 
+N_UNIQUE_WORDS = 10000  # cut texts after this number of words
+MAXLEN = 200
+BATCH_SIZE = 1024
+
 # https://stackoverflow.com/a/56243777/3249688
 np_load_old = np.load
 np.load = lambda *args, **kwargs: np_load_old(*args, allow_pickle=True, **kwargs)
 
-n_unique_words = 10000  # cut texts after this number of words
-maxlen = 200
-batch_size = 1024
-
-(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=n_unique_words)
-x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
-x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
+(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=N_UNIQUE_WORDS)
+x_train = sequence.pad_sequences(x_train, maxlen=MAXLEN)
+x_test = sequence.pad_sequences(x_test, maxlen=MAXLEN)
 y_train = np.array(y_train)
 y_test = np.array(y_test)
 
+np.load = np_load_old
+
 model = Sequential()
-model.add(Embedding(n_unique_words, 128, input_length=maxlen))
+model.add(Embedding(N_UNIQUE_WORDS, 128, input_length=MAXLEN))
 model.add(Bidirectional(LSTM(64)))
 model.add(Dropout(0.5))
 model.add(Dense(1, activation="sigmoid"))
@@ -30,9 +32,7 @@ model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"]
 
 print("Train...")
 model.fit(
-    x_train, y_train, batch_size=batch_size, epochs=4, validation_data=[x_test, y_test]
+    x_train, y_train, batch_size=BATCH_SIZE, epochs=4, validation_data=[x_test, y_test]
 )
 print("Evaluate...")
-model.evaluate(x_test, y_test, batch_size=batch_size)
-
-np.load = np_load_old
+model.evaluate(x_test, y_test, batch_size=BATCH_SIZE)
