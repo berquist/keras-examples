76b4742d6216f34c340c7bdcd17c079e6afa7f81
Eric Berquist <eric.berquist@gmail.com>
2019-08-21 14:05:50 -0400

    Fix for unpickling with newest NumPy versions

--- bilstm_imdb_example.py
+++ bilstm_imdb_example.py
@@ -6,10 +6,13 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Embedding, LSTM, Bidirectional
 from keras.datasets import imdb
 
+# https://stackoverflow.com/a/56243777/3249688
+np_load_old = np.load
+np.load = lambda *args, **kwargs: np_load_old(*args, allow_pickle=True, **kwargs)
 
 n_unique_words = 10000  # cut texts after this number of words
 maxlen = 200
-batch_size = 128
+batch_size = 1024
 
 (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=n_unique_words)
 x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
@@ -29,3 +32,7 @@ print("Train...")
 model.fit(
     x_train, y_train, batch_size=batch_size, epochs=4, validation_data=[x_test, y_test]
 )
+print("Evaluate...")
+model.evaluate(x_test, y_test, batch_size=batch_size)
+
+np.load = np_load_old
