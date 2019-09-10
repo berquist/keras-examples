a932d6ed796664520a6ac9b12b989227d2b9538e
Eric Berquist <berquist@isi.edu>
2019-09-10 16:22:53 -0400

    Simplify 7695 issue to compare LSTM w/ BiLSTM

--- issue_7695.py
+++ issue_7695.py
@@ -1,16 +1,28 @@
-from keras.layers import Embedding, LSTM, Dense, Input, Masking
-from keras.layers.wrappers import Bidirectional
-from keras.models import Model
 import numpy as np
-import tensorflow as tf
+from keras import Model
+from keras.layers import Bidirectional, LSTM, Input, Masking
+
+np.set_printoptions(linewidth=200)
 
 ## https://github.com/keras-team/keras/issues/7695
 
-vec = np.random.randn(3, 5)
-inp = Input((3,))
+n_samples = 2
+dx = 2
+dy = 3
+mask_value = -1
+
+X = np.random.randint(5, size=(n_samples, dx, dy))
+X[1, 0, :] = mask_value
+
+inp = Input(shape=(dx, dy))
 x = Masking(mask_value=-1.0)(inp)
-x = Embedding(3, 5, weights=[vec], input_length=3, trainable=False)(x)
-x = Bidirectional(LSTM(10, return_sequences=True))(x)
-sess = tf.Session()
-sess.run(tf.global_variables_initializer())
-print(sess.run(x, {inp: [[0, 2, -1], [1, -1, -1]]}))
+
+lstm = LSTM(4, return_sequences=True)(x)
+model_1 = Model(inputs=inp, outputs=lstm)
+model_1.summary()
+print(model_1.predict(X))
+
+bilstm = Bidirectional(LSTM(4, return_sequences=True), merge_mode="concat")(x)
+model_2 = Model(inputs=inp, outputs=bilstm)
+model_2.summary()
+print(model_2.predict(X))
